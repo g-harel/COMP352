97e4e42b8594a306fe27c8002284f913b2acdbc8
Gabriel <pabombs@gmail.com>
2016-06-03 23:41:13 -0400

    added comments and started written part

--- src/Assignment2/Driver.java
+++ src/Assignment2/Driver.java
@@ -2,26 +2,34 @@ package Assignment2;
 
 public class Driver {
     public static void main(String[] args) {
-        Position[] poss = new Position[10];
-        for (int i = 0; i < poss.length; i++) {
-            poss[i] = new Position(poss.length-i);
-        }
         try {
-            NodeList list = new NodeList();
-            list.setExpansionRule((Math.random() > 0.5) ? 'd' : 'c');
-            poss[0] = list.addFirst(1);
-            poss[1] = list.addFirst(0);
-            poss[2] = list.addLast(1);
-            list.swap(0, 1);
-            list.set(poss[2], 2);
-            list.delete(poss[0]);
-            list.truncate();
-            poss[3] = list.addFirst(3);
-            poss[4] = list.addLast(4);
-            poss[5] = list.addBefore(poss[4], 5);
-            poss[6] = list.addAfter(poss[1], 6);
-            System.out.println("first: " + list.first());
-            System.out.println("last: " + list.last());
+//            Position[] poss = new Position[10];
+//            for (int i = 0; i < poss.length; i++) {
+//                poss[i] = new Position(poss.length-i);
+//            }
+//            NodeList list = new NodeList();
+//            list.setExpansionRule((Math.random() > 0.5) ? 'd' : 'c');
+//            poss[0] = list.addFirst(1);
+//            poss[1] = list.addFirst(0);
+//            poss[2] = list.addLast(1);
+//            list.swap(0, 1);
+//            list.set(poss[2], 2);
+//            list.delete(poss[0]);
+//            list.truncate();
+//            poss[3] = list.addFirst(3);
+//            poss[4] = list.addLast(4);
+//            poss[5] = list.addBefore(poss[4], 5);
+//            poss[6] = list.addAfter(poss[1], 6);
+//            System.out.println("first: " + list.first());
+//            System.out.println("last: " + list.last());
+            NodeList big = new NodeList();
+            long startTime = System.nanoTime();
+            big.setExpansionRule((Math.random() > 0.5) ? 'd' : 'c');
+            for (int i = 0; i < 100000; i++) {
+                big.addLast(i);
+            }
+            //System.out.println(big.toString());
+            System.out.println((System.nanoTime() - startTime)/1000000);
         } catch (Exception e) {
             e.printStackTrace(System.out);
         }
--- src/Assignment2/NodeList.java
+++ src/Assignment2/NodeList.java
@@ -3,15 +3,19 @@ package Assignment2;
 public class NodeList {
 
     private Position[] data;
-    private int last_pos = -1;
     private char rule;
+	// variable to track the index of the last position
+    private int last_pos = -1;
 
+	// constructor
     public NodeList() {
+		// create the underlying array with a size of 2
         data = new Position[2];
         rule = 'd';
         System.out.println("New NodeList\n>" + this.toString() + "\n");
     }
 
+	// toString method to display contents
     @Override
     public String toString() {
         String output = "";
@@ -22,6 +26,7 @@ public class NodeList {
         return output;
     }
 
+	// checks if the array can fit another value
     private boolean fits() {
         if ((last_pos+2.0)/data.length < 0.8) {
             return true;
@@ -30,6 +35,7 @@ public class NodeList {
         }
     }
 
+	// creates an array of a bigger size according to the rule variable
     private Position[] expand() {
         int length;
         if (rule =='d') {
@@ -41,6 +47,7 @@ public class NodeList {
         return temp;
     }
 
+	// returns the Position of the first array index
     public Position first() throws EmptyListException {
         if (last_pos > 0) {
             return data[0];
@@ -49,6 +56,7 @@ public class NodeList {
         }
     }
 
+	// returns the Position of the last array index
     public Position last() throws EmptyListException {
         if (last_pos >= 0) {
             return data[last_pos];
@@ -57,6 +65,7 @@ public class NodeList {
         }
     }
 
+	// looks for the specified Position and returns the one before it
     public Position prev(Position pos) throws ArrayIndexOutOfBoundsException {
         for (int i = 1; i < last_pos; i++) {
             if (data[i] == pos) {
@@ -66,6 +75,7 @@ public class NodeList {
         throw new ArrayIndexOutOfBoundsException();
     }
 
+	// looks for the specified Position and returns the one before it
     public Position next(Position pos) throws ArrayIndexOutOfBoundsException {
         for (int i = 0; i <= last_pos-1; i++) {
             if (data[i] == pos) {
@@ -75,6 +85,7 @@ public class NodeList {
         throw new ArrayIndexOutOfBoundsException();
     }
 
+	// finds the Position and changes its element value
     public int set(Position pos, int val) throws ArrayIndexOutOfBoundsException {
         for (int i = 1; i <= last_pos; i++) {
             if (data[i] == pos) {
@@ -87,7 +98,9 @@ public class NodeList {
         throw new ArrayIndexOutOfBoundsException();
     }
 
+	// adds to the front of the array by creating a new array and remapping all the contents
     public Position addFirst(int val) {
+		// creating temp array to transfer values
         Position[] temp;
         if (!fits()) {
             temp = expand();
@@ -95,6 +108,7 @@ public class NodeList {
             temp = new Position[data.length];
         }
         temp[0] = new Position(val);
+		// filling in all values after index 0
         for (int i = 0; i <= last_pos; i++) {
             temp[i+1] = data[i];
         }
@@ -103,8 +117,9 @@ public class NodeList {
         System.out.println("AddFirst: " + val + "\n>" + this.toString() + "\n");
         return temp[0];
     }
-
+	// adds to the back of the array by creating a new array and remapping all the contents
     public Position addLast(int val) {
+		// creating a new array and copying the original if an extra value doesn't fit
         if (!fits()) {
             Position[] temp = expand();
             for (int i = 0; i <= last_pos; i++) {
@@ -112,35 +127,33 @@ public class NodeList {
             }
             data = temp;
         }
-        last_pos++;
+		last_pos++;
         data[last_pos] = new Position(val);
         System.out.println("AddLast: " + val + "\n>" + this.toString() + "\n");
         return data[last_pos];
     }
-
+	// adds a new Position at the index after the specified Position
     public Position addBefore(Position pos, int val) throws ArrayIndexOutOfBoundsException {
+		// creating new array to transfer values
         Position[] temp;
-        int index = -1;
+        boolean flag = false;
         if (!fits()) {
             temp = expand();
         } else {
             temp = new Position[data.length];
         }
-        for (int i = 0; i <= last_pos; i++) {
-            if (data[i] == pos) {
-                index = i;
-                break;
-            }
-        }
-        if (index == -1) {
-            throw new ArrayIndexOutOfBoundsException();
-        }
+		// filling all values
         for (int i = 0, j = 0; i <= last_pos; i++, j++) {
-            if (j == index) {
+			// adding the new Position
+            if (data[i] == pos) {
                 temp[j] = new Position(val);
                 j++;
+                flag = true;
             }
             temp[j] = data[i];
+        }
+        if (!flag) {
+            throw new ArrayIndexOutOfBoundsException();
         }
         data = temp;
         last_pos++;
@@ -148,29 +161,28 @@ public class NodeList {
         return pos;
     }
 
+	// adds a new Position at the index before the specified Position
     public Position addAfter(Position pos, int val) throws ArrayIndexOutOfBoundsException {
+		// creating new array to transfer values
         Position[] temp;
-        int index = -1;
+        boolean flag = false;
         if (!fits()) {
             temp = expand();
         } else {
             temp = new Position[data.length];
         }
-        for (int i = 0; i <= last_pos; i++) {
-            if (data[i] == pos) {
-                index = i+1;
-                break;
-            }
-        }
-        if (index == -1) {
-            throw new ArrayIndexOutOfBoundsException();
-        }
+		// filling in all values
         for (int i = 0, j = 0; i <= last_pos; i++, j++) {
-            if (j == index) {
-                temp[j] = new Position(val);
-                j++;
-            }
             temp[j] = data[i];
+			// adding the new Position
+            if (data[i] == pos) {
+                j++;
+                temp[j] = new Position(val);
+                flag = true;
+            }
+        }
+        if (!flag) {
+            throw new ArrayIndexOutOfBoundsException();
         }
         data = temp;
         last_pos++;
@@ -178,30 +190,27 @@ public class NodeList {
         return pos;
     }
 
+	// removes the specified Position
     public void delete(Position pos) throws ArrayIndexOutOfBoundsException{
-        Position[] temp;
-        int index = -1;
-        temp = new Position[data.length];
-        for (int i = 0; i <= last_pos; i++) {
-            if (data[i] == pos) {
-                index = i;
-                break;
-            }
-        }
-        if (index == -1) {
-            throw new ArrayIndexOutOfBoundsException();
-        }
+        Position[] temp = new Position[data.length];
+        boolean flag = false;
+		// filling in all values to the new array except the one with the Position that is beind deleted
         for (int i = 0, j = 0; i <= last_pos; i++, j++) {
-            if (j == index) {
+            if (data[i] == pos) {
                 i++;
+                flag = true;
             }
             temp[j] = data[i];
+        }
+        if (!flag) {
+            throw new ArrayIndexOutOfBoundsException();
         }
         data = temp;
         last_pos--;
         System.out.println("delete: " + pos.element() + "\n>" + this.toString() + "\n");
     }
 
+	// swaps the values stored in two Positions in the array
     public void swap(int p1, int p2) throws ArrayIndexOutOfBoundsException{
         int temp = data[p1].element();
         data[p1].set(data[p2].element());
@@ -209,8 +218,10 @@ public class NodeList {
         System.out.println("Swap: " + p1 + " and " + p2 + "\n>" + this.toString() + "\n");
     }
 
+	// truncates the array to the minimum possible size
     public void truncate() {
         Position[] temp = new Position[last_pos+1];
+		// filling in the new values
         for (int i = 0; i <= last_pos; i++) {
             temp[i] = data[i];
         }
@@ -218,8 +229,10 @@ public class NodeList {
         System.out.println("Truncate:\n>" + this.toString() + "\n");
     }
 
+	// sets the expansion rule to the one specified
     public void setExpansionRule(char rule) {
         if (rule == 'd' || rule == 'c') {
+            System.out.println(rule);
             this.rule = rule;
         }
     }
